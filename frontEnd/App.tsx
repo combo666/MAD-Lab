d70fa7dabddada02aa06ab7794d60c9983e3cf85
combo666 <shadat1124@gmail.com>
2023-10-29 05:36:39 +0600

    Chat fetcher Front End Layout design

--- frontEnd/App.tsx
+++ frontEnd/App.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { View,Text,StyleSheet } from "react-native";
 import { counterEvent } from "react-native/Libraries/Performance/Systrace";
-import CounterPage from "./src/pages/counterPage";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+import CounterPage from "./src/pages/counterPage";
 import LoginPage from "./src/pages/LoginPage";
 import SettingsPage from "./src/pages/SettingsPage"
 import NationalizePage from "./src/pages/NationalizePage"
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import ChatsPage from "./src/pages/ChatsPage";
 
 const stack = createStackNavigator();
 const bottom_tab = createBottomTabNavigator();
@@ -24,10 +27,41 @@ const BasicDashboardScreen =() =>{
 const App = () =>{
   return(
     <NavigationContainer>
-      <bottom_tab.Navigator>
-        <bottom_tab.Screen name="Dashboard" component={BasicDashboardScreen}/>
+      <bottom_tab.Navigator
+        initialRouteName="Chats"
+        screenOptions={{
+          tabBarStyle: {
+            backgroundColor: "#1B1B1B",
+            alignItems: 'center',
+            borderTopWidth: 0, 
+          },
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: '#E0FFB4',
+          tabBarInactiveTintColor: "grey",
+          title: "Soil Society",
+          headerPressOpacity: 5,
+          headerShown: false
+        }}
+      >
+        <bottom_tab.Screen name="Dashboard" component={BasicDashboardScreen}
+          options={{
+            tabBarLabel:"Dashboard",
+            tabBarIcon: ({focused})=> <MaterialCommunityIcons name={focused ? "post": "post-outline"} size={focused? 30: 20} color={focused ?"#DDFF54" : 'white'}/>,
+            tabBarLabelStyle:{fontSize: 14, fontWeight: 'bold'},
+            tabBarActiveTintColor: "#E0FFB4"
+          }}
+        />
         <bottom_tab.Screen name="Settings" component={SettingsPage}/>
         <bottom_tab.Screen name="NationalizePage" component={NationalizePage}/>
+        
+        <bottom_tab.Screen name="Chats" component={ChatsPage}
+          options={{
+            tabBarLabel:"Dashboard",
+            tabBarIcon: ({focused})=> <MaterialCommunityIcons name={focused ? "chat": "chat-outline"} size={focused? 30: 20} color={focused ?"#DDFF54" : 'white'}/>,
+            tabBarLabelStyle:{fontSize: 14, fontWeight: 'bold'},
+            tabBarActiveTintColor: "#E0FFB4"
+          }}
+        />
       </bottom_tab.Navigator>
     </NavigationContainer>
   );
---
+++ frontEnd/src/pages/ChatsPage.tsx
@@ -0,0 +1,146 @@
+import React, {useState} from 'react';
+import { View,Text,SafeAreaView,TextInput,TouchableOpacity,ScrollView   } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { StyleSheet } from 'react-native';
+
+const Stack = createStackNavigator();
+
+function ChatsPAge() {
+
+    const [selectedButton, setSelectedButton] = useState(null);
+
+    const handleButtonPress = (buttonId) => {
+        setSelectedButton(buttonId);
+    };
+
+    return (
+        <SafeAreaView style={{flex: 1, backgroundColor: "#1B1B1B", justifyContent: 'flex-start', paddingTop: 48, }}>
+            <Text style={{
+                    color: "#D8E9A8",
+                    fontFamily: "Arimo Hebrew Subset",
+                    fontSize: 30,
+                    fontStyle: "normal",
+                    fontWeight: "700",
+                    paddingLeft: 20
+                    }}>
+                    GreenChats
+            </Text>
+
+            <View style={{
+                backgroundColor: "#FEFEFE", 
+                height: 137,
+                marginTop: 15,
+                marginLeft: 5,
+                marginRight: 5,
+                borderRadius: 20
+
+                
+            }}>
+                <View>
+                    
+                </View>
+
+                <View style={{
+                    backgroundColor: "#D8E9A8",
+                    height: 46,
+                    flexShrink: 0,
+                    marginTop: 81,
+                    marginLeft: 20,
+                    marginRight: 20,
+                    borderRadius: 20
+                    
+                }}>
+                    <ScrollView
+                        horizontal={true} // Enable horizontal scrolling
+                        style={{
+                            paddingLeft: 13 
+
+                        }}
+                    >
+                        <TouchableOpacity
+                            onPress={() => handleButtonPress(1)}
+                            style={{
+                                backgroundColor: selectedButton === 1 ? '#64ABBC' : '#D8E9A8', 
+                                borderRadius: 20,
+                                marginTop: 3,
+                                marginRight: 5,
+                                
+                                height: 40,
+                                padding: 10,
+                            }}
+                        >
+                            <Text style={{
+                                color: selectedButton === 1 ? '#FEFEFE' : '#1B1B1B',
+                                marginLeft:5,
+                                marginRight:5,
+                            }}>Global</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity
+                            onPress={() => handleButtonPress(2)}
+                            style={{
+                                backgroundColor: selectedButton === 2 ? '#64ABBC' : '#D8E9A8', 
+                                borderRadius: 20,
+                                marginTop: 3,
+                                marginRight: 5,
+                                
+                                height: 40,
+                                padding: 10,
+                            }}
+                        >
+                            <Text style={{
+                                color: selectedButton === 2 ? '#FEFEFE' : '#1B1B1B',
+                                marginLeft:5,
+                                marginRight:5,
+                            }}>Local</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity
+                            onPress={() => handleButtonPress(3)}
+                            style={{
+                                backgroundColor: selectedButton === 3 ? '#64ABBC' : '#D8E9A8', 
+                                borderRadius: 20,
+                                marginTop: 3,
+                                marginRight: 5,
+                                
+                                height: 40,
+                                padding: 10,
+                            }}
+                        >
+                            <Text style={{
+                                color: selectedButton === 3 ? '#FEFEFE' : '#1B1B1B',
+                                marginLeft:5,
+                                marginRight:5,
+                            }}>Experts</Text>
+                        </TouchableOpacity>
+                        
+                        
+
+                        
+
+                    </ScrollView>
+
+                </View>
+                
+                <TextInput
+                    style={{
+                    backgroundColor: '#FEFEFE',
+                    padding: 10,
+                    paddingLeft: 20,
+                    height: 50,
+                    borderRadius: 20,
+                    marginTop: 35,
+                    marginLeft: 15,
+                    marginRight: 15,
+                    fontFamily:"Arimo Hebrew Subset",
+                    }}
+                    placeholder="Search For Local Chats..."
+                />
+
+            </View>
+            
+        </SafeAreaView>
+    );
+}
+
+export default ChatsPAge;
